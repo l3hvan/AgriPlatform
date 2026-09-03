1eb553c4641b5244ce29dac8eb6d76d4cb8c819e
Babu-ops <lehvancsbs2022@gmail.com>
2026-09-03 08:26:03 +0530

    base project

---
+++ check_setup.py
@@ -0,0 +1,11 @@
+import django
+import sklearn
+import pandas
+import numpy
+import joblib
+
+print("Django:", django.get_version())
+print("scikit-learn:", sklearn.__version__)
+print("pandas:", pandas.__version__)
+print("numpy:", numpy.__version__)
+print("All imports successful!")
---
+++ grains_plants/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GrainsPlantsConfig(AppConfig):
+    name = 'grains_plants'
