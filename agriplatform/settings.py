aac4c01aa83b2c2b297d39947515337d305b08e9
Babu-ops <lehvancsbs2022@gmail.com>
2026-09-24 18:46:45 +0530

    allow host changed back

--- agriplatform/settings.py
+++ agriplatform/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
+
 # Application definition
 
 INSTALLED_APPS = [
